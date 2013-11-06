b1f2039f0035af1497fdba05ecbaf18169766ac9
Chaiyawat Nunes <chaiyawat.nunes@gmail.com>
2013-11-05 16:51:02 -0800

    Update response_ironbot.js

--- src/server/game/ironbot/lib/response_ironbot.js
+++ src/server/game/ironbot/lib/response_ironbot.js
@@ -24,8 +24,11 @@ var dictionary = {};
 /* error. */dictionary.error = "Hello, BLEEP, Syntax error";
 /* divided. */dictionary.divided = "Divided by zero, memory problem";
 /* where. */dictionary.where = "I am in your house, stealing your money";
-/* robot */dictionary.robot = "Do you want ME to call YOU a robot?";
-/* idiot */dicitonary.idiot = "3.14159265358979323846264338327950288419796939937510"; //this is from PURE memory
+/* robot. */dictionary.robot = "Do you want ME to call YOU a robot?";
+/* idiot. */dicitonary.idiot = "3.14159265358979323846264338327950288419796939937510"; //this is from PURE memory
+/* service. */dictionary.service = "Ironbot, at your service";
+/* dismissed. */dicitonary.dismissed = "Ironbot, dismissed";
+/* poor. */dicitonary.poor = "Helping the poor, one bot at a time";
 
 module.exports = dictionary;
 
